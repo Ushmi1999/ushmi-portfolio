7890f43a7d579d4deda3a596976a058245ca9ead
Ushmi1999 <nimmiushmi@gmail.com>
2025-08-14 21:07:17 +0530

    Update contact info

--- src/Contact.js
+++ src/Contact.js
@@ -4,8 +4,8 @@ function Contact() {
       <h2>Contact</h2>
       <ul>
         <li>Email: nimmiushmi@gmail.com</li>
-        <li>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">yourprofile</a></li>
-        <li>Phone: +94 765275654</li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/ushmi-nimsara-6a30042a8" target="_blank" rel="noopener noreferrer">Ushmi Nimsara</a></li>
+        <li>Phone: +94 76 527 5654</li>
       </ul>
     </section>
   );
